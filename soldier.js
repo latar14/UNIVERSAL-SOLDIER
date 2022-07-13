de15195a06a50386dc99671f9e369db3081a4704
Ashwash <latar14@bk.ru>
2022-07-13 11:15:01 +0300

    Сделал код-ревью

--- soldier.js
+++ soldier.js
@@ -1,34 +1,34 @@
 const soldier = {
-    name: 'bot',
-    helth: 10,
-    weapon: {
-        weaponName: 'FNScar',
-        ammunition: 30,
-    },
-    supplies: 3,
-    getFire: function () {
-        if (this.ammunition = 0) {
-            'обойма пуста. Перезарядитесь.'
-        }
-        { 
-            this.ammunition--;
-            return 'бах-бах'
-        }
-    },
-    getRecharge: function () {
-        if (this.supplies = 0) {
-            console.log('не осталось припасов')
-        } else {
-        this.ammunition = 30;
-        this.supplies--; 
-        return 'перезарядка...'
-        }
-    },
-    getHurt: function () {
-        if (this.helth = 0) {
-            console.log('Ты проиграл')
-        } else {
-        this.helth--, this.helth = 0
-        }
-    },
-}
+  name: "bot",
+  helth: 10,
+  weapon: {
+    weaponName: "FNScar",
+    ammunition: 30,
+  },
+  supplies: 3,
+  getFire: function () {
+    if ((this.ammunition = 0)) {
+      ("обойма пуста. Перезарядитесь.");
+    }
+    {
+      this.ammunition--;
+      return "бах-бах";
+    }
+  },
+  getRecharge: function () {
+    if ((this.supplies = 0)) {
+      console.log("не осталось припасов");
+    } else {
+      this.ammunition = 30;
+      this.supplies--;
+      return "перезарядка...";
+    }
+  },
+  getHurt: function () {
+    if ((this.helth = 0)) {
+      console.log("Ты проиграл");
+    } else {
+      this.helth--, (this.helth = 0);
+    }
+  },
+};
