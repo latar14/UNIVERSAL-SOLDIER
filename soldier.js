7f24d4dd58663567cdaf4077a9a225f3d1c6f4fe
Ashwash <latar14@bk.ru>
2022-07-13 10:17:30 +0300

    Создание солдата

---
+++ soldier.js
@@ -0,0 +1,14 @@
+const soldier {
+    name: 'bot',
+    helth: 10,
+    weapon: {
+        weaponName: 'FNScar',
+        ammunition: 30,
+
+    },
+    supplies: 3,
+    getFire: function,
+    getRecharge: function,
+    getHurt: function,
+
+}
